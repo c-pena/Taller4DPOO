1486993cd9771924ab6f5c94ded5d457cb620b81
Carlos <c.penaa@uniandes.edu.co>
2023-10-27 12:12:54 -0500

    v0.04 - Top funcional

--- src/GUI/PanelOpciones.java
+++ src/GUI/PanelOpciones.java
@@ -1,14 +1,21 @@
 package GUI;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Collection;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
+
+import modelo.RegistroTop10;
+import modelo.Top10;
 
 public class PanelOpciones extends JPanel {
 
@@ -16,11 +23,13 @@ public class PanelOpciones extends JPanel {
     private JButton btnReiniciar;
     private JButton btnTop10;
     private JButton btnCambiarJugador;
+    private Top10 top10;
 
+    public PanelOpciones(Top10 top10) {
+        this.top10 = top10;
 
-    public PanelOpciones() {
         setLayout(new GridLayout(15,1));
-        setPreferredSize(new Dimension(200, 720));
+        setPreferredSize(new Dimension(230, 720));
         Color colorBtn = new Color(46,153,229);
 
         btnNuevo = new JButton("NUEVO");
@@ -40,6 +49,19 @@ public class PanelOpciones extends JPanel {
         btnTop10.setOpaque(true);
         btnTop10.setBorderPainted(false);
         btnTop10.setForeground(Color.WHITE);
+        ActionListener actionTop10 = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               Collection<RegistroTop10> topList = top10.darRegistros();
+               JDialog dialog = new JDialog((JFrame) getTopLevelAncestor(), "Top 10", true);
+                dialog.setSize(200, 100);
+                dialog.setLocationRelativeTo(getTopLevelAncestor());
+                dialog.add(new JLabel("This is a dialog"));
+                dialog.setVisible(true);
+            }
+        };
+        btnTop10.addActionListener(actionTop10);
+
 
         btnCambiarJugador = new JButton("CAMBIAR JUGADOR");
         btnCambiarJugador.setBackground(colorBtn);
--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -2,13 +2,22 @@ package GUI;
 
 import javax.swing.JFrame;
 import javax.swing.border.Border;
+
+import modelo.Top10;
+
 import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 
 public class VentanaPrincipal extends JFrame {
 
     private PanelOpciones panelEste;
     private PanelModo panelNorte;
     private PanelEstado panelSur;
+    private Top10 top10;
     //private PanelTablero panelTablero;
 
     public VentanaPrincipal() {
@@ -16,11 +25,29 @@ public class VentanaPrincipal extends JFrame {
         setSize(960, 720);
         setLocationRelativeTo(null);
         setResizable(false);
+        top10 = new Top10();
+        String workingDir = System.getProperty("user.dir");
+        String filePath = workingDir + File.separator + "data" + File.separator + "top10.csv";
+        File top10CSV = new File(filePath);
+        top10.cargarRecords(top10CSV);
+        System.out.println("Records cargados");
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                try {
+                    top10.salvarRecords(top10CSV);
+                    System.out.println(top10.darRegistros());
+                    System.out.println("Records salvados");
+                } catch (FileNotFoundException | UnsupportedEncodingException e1) {
+                    e1.printStackTrace();
+                }
+                }
+        });
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setLayout(new BorderLayout());
 
-        panelEste = new PanelOpciones();
+        panelEste = new PanelOpciones(top10);
         add(panelEste, BorderLayout.EAST);
 
         panelNorte = new PanelModo();
@@ -28,8 +55,6 @@ public class VentanaPrincipal extends JFrame {
 
         panelSur = new PanelEstado();
         add(panelSur, BorderLayout.SOUTH);
-
-
 
         setVisible(true);
     }
